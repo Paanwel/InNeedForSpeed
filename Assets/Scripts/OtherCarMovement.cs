c6320b49d23c5218bcfa1b6161caac229640c21c
Paanwel <paanwel123@gmail.com>
2024-11-04 19:43:27 +0100

    init

---
+++ Assets/Scripts/OtherCarMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherCarMovement : MonoBehaviour
+{
+    public Transform transform;
+    public float speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform transform;
+    public float speed = 5.0f;
+    public float rotationSpeed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 47), rotationSpeed * Time.deltaTime);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, -47), rotationSpeed * Time.deltaTime);
+        }
+        if (transform.rotation.z !=90)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), rotationSpeed * Time.deltaTime);
+        }
+    }
+    void Clamp()
+    {
+        // unity inbuild feature
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, -2.71f, 2.8f);
+        transform.position = pos;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Cars")
+        Debug.Log("Kolizja!!!!!!!!!!");
+    }
+}
