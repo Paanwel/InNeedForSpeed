583053b408cb2d64b759dfa157a866b9ef008c14
Paanwel <paanwel123@gmail.com>
2024-11-28 09:23:43 +0100

    commit z 28.11 z menu screen-em

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName = "Main";
+
+    [Header("References")]
+    [SerializeField]
+    private Button startButton;
+
+    [SerializeField]
+    private CanvasGroup mainScreen, settingsScreen;
+
+
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    public void ToggleSettings(bool enabled)
+    {
+        mainScreen.gameObject.SetActive(!enabled);
+        settingsScreen.gameObject.SetActive(enabled);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+    }
+    private void Awake()
+    {
+        if(startButton != null)
+        {
+            startButton.onClick.AddListener(StartGame);
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+}
