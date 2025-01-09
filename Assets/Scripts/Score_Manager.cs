2edd42928eb572beb7539c59f137c1d3a11bde48
Paanwel <paanwel123@gmail.com>
2025-01-09 19:57:47 +0100

    commit z 09.01

--- Assets/Scripts/Score_Manager.cs
+++ Assets/Scripts/Score_Manager.cs
@@ -3,25 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Score_Manager : MonoBehaviour
 {
     public int score = 0;
     public int highScore;
-    public static int lastScore = 0;
+
 
     public Text scoreText;
+    public Text highScoreText;
+    
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(Score());
-        StartCoroutine(Reload());
 
         highScore = PlayerPrefs.GetInt("high_score", 0);
-        Debug.Log("High Score Stored: " + PlayerPrefs.GetInt("high_score", highScore));
-        Debug.Log("High Score Get: " + highScore);
-
-        Debug.Log("Last Score: " + lastScore);  
+        highScoreText.text = "HighScore: " + highScore.ToString();
+        
     }
 
     // Update is called once per frame
@@ -44,13 +44,9 @@ public class Score_Manager : MonoBehaviour
             yield return new WaitForSeconds(0.8f);
             score = score + 1;
             Debug.Log("Score: " + score);
-            lastScore = score;
+            
         }
     }
 
-    IEnumerator Reload()
-    {
-        yield return new WaitForSeconds(Random.Range(5, 10));
-        SceneManager.LoadScene("Game");
-    }
+
 }
